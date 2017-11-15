5c0d8f7ff6c99445a95fec4c84bc8a6bd85e9076
root <root@ip-172-31-14-128.eu-west-2.compute.internal>
2017-11-15 03:23:17 +0000

    no check

--- merge_upstream.sh
+++ merge_upstream.sh
@@ -3,26 +3,26 @@
 # This script helps to automatically synchronise a GitHub fork to the upstream repository
 
 # Fetch upstream changes
-git remote add upstream https://github.com/ethereum/meteor-dapp-wallet
+git remote add upstream https://github.com/tissazak/meteor-dapp-wallet
 git fetch upstream
 
 # Prune deleted branches
 git fetch -p
 
 # Swap to master branch
-git checkout master
+git checkout develop
 
 # Merge in the changes from upstream/master
-git merge upstream/master
+git merge upstream/develop
 
 # Check if any branches need pruning
-git branch --merged master | grep -v 'master$'
+git branch --merged develop | grep -v 'develop$'
 
 if [ $? -eq 1 ]; then
         echo "No local branches need deleting"
 else
         echo "Local branches need deleting"
-        git branch --merged master | grep -v 'master$' | xargs git branch -d
+        git branch --merged develop | grep -v 'develop$' | xargs git branch -d
 fi
 
 echo "Finished!"
